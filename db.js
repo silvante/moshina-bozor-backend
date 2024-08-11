9b3aa7ae95b6014af836f24da1c678478539c222
silvante <khamidov.ko@gmail.com>
2024-08-11 12:58:30 +0500

    fix

--- db.js
+++ db.js
@@ -9,3 +9,4 @@ module.exports = function connection() {
     console.log("could not connect to database");
   }
 };
+// exporting
--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: process.env.DOMAIN,
   })
 );
 app.use(cp());
